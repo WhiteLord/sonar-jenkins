63b9d6a9b0c58cd7f1ccda9cb534fe7ecba3fd2a
workstation.local <workstation@localworkstation.local>
2020-03-31 14:38:47 +0300

    Adding a few more test cases

--- business-service/src/main/java/org/elementalsource/app/App.java
+++ business-service/src/main/java/org/elementalsource/app/App.java
@@ -6,8 +6,11 @@ package org.elementalsource.app;
  */
 public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public static String callHello() {
+        return "Hello world";
     }
 }
--- business-service/src/main/java/org/elementalsource/business/App.java
+++ business-service/src/main/java/org/elementalsource/business/App.java
@@ -10,4 +10,7 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+    public static String callHello() {
+        return "Hello world";
+    }
 }
